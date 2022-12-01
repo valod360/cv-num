8afa60b4dbce92ae1caa26b51c4454d39bba1458
valod360 <83767512+valod360@users.noreply.github.com>
2022-12-01 16:52:08 +0100

    vaisseau fait petite instruction a faire ajouter

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,17 +2,30 @@ const scrollContainer = document.querySelector('main');
 let flame = document.getElementById('flammeVaisseau')
 let vaisseau = document.getElementById('vaisseau')
 let timer = null;
+let animationMoment = 0;
 
+/* recuperer le deltay et lui attribuer une valeur puis la retirer si il scroll en negatif une fois fait
+faire les animation selon la position du scroll */
+
+/* faire une function qui triger avant qu'il arrive sur la div suivante*/
 
 scrollContainer.addEventListener('wheel', (evt) => {
+
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY
+    if (evt.deltaY == 100) {
+        ++animationMoment;
+    } if (evt.deltaY == -100) {
+        --animationMoment;
+    }
+    console.log(animationMoment)
+
     flame.style.display = 'block';
     vaisseau.style.display = 'none';
-    vaisseau.style.animation = 'animate 1s';
-    vaisseau.classList.add('rotate');
-    vaisseau.removeAttribute('id');
-    
+    /*  vaisseau.style.animation = 'animate 1s';
+     vaisseau.classList.add('rotate');
+     vaisseau.removeAttribute('id'); */
+
 
 });
 
